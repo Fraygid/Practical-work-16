9d0a0920953fd5a4d2b2b343547dc0c0e2bfd41d
Fraygid <136465592+Fraygid@users.noreply.github.com>
2026-02-09 12:28:34 +0300

    Update №5 "Подозрительная активность".py

--- №5 "Подозрительная активность".py
+++ №5 "Подозрительная активность".py
@@ -1,1 +1,10 @@
-asd
+adress_1 = set(input('Введите IP-адресса 1: ').split())
+adress_2 = set(input('Введите IP-адресса 2: ').split())
+adress_3 = set(input('Введите IP-адресса 3: ').split())
+
+all_adress = adress_1 | adress_2 | adress_3
+in_all_three = adress_1 & adress_2 & adress_3
+
+suspicious = all_adress - in_all_three
+
+print(' '.join(sorted(suspicious)))
